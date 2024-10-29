e6268389c2f2776ab0af8e4dd3f8f38be14e8a30
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-10-29 07:10:44 -0500

    Agregar archivos de proyecto.

---
+++ Cortez_Enzo_Evaluacion_P_I/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cortez_Enzo_Evaluacion_P_I.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
